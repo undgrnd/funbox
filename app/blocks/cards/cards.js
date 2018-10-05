5b334491c1e1fae2282f8131c70b09896367b507
Yuri Mikhin <mihin@corsoagency.info>
2018-10-05 16:09:23 +0300

    правки мобильной версии

--- app/blocks/cards/cards.js
+++ app/blocks/cards/cards.js
@@ -1,48 +1,49 @@
 'use strict';
 
-const cards = document.querySelectorAll('.card');
-const weights = document.querySelectorAll('.card__weight');
-const links = document.querySelectorAll('.link');
+const cards = document.querySelectorAll('.cards__card');
+const weights = document.querySelectorAll('.cards__weight');
+const links = document.querySelectorAll('.cards__link');
+let lastClickedCard;
 
-// ie11 foreach polyfill
-if ('NodeList' in window && !NodeList.prototype.forEach) {
-	console.info('polyfill for IE11');
-	NodeList.prototype.forEach = function (callback, thisArg) {
-		thisArg = thisArg || window;
-		for (let i = 0; i < this.length; i++) {
-			callback.call(thisArg, this[i], i, this);
-		}
-	};
+function handler() {
+	lastClickedCard.className = 'cards__card cards__card_active';
+	lastClickedCard.removeEventListener('mouseleave', handler);
 }
 
-cards.forEach(function (card, index) {
-	// отключенные карточки не участвуют
-	if (!card.classList.contains('card--disabled')) {
-		// изменить класс по клику
-		card.addEventListener('click', function () {
-			// убрать класс активности
-			if (card.classList.contains('card--active') ||
-				card.classList.contains('card--active-first')) {
-				card.className = 'card';
-				weights[index].className = 'card__weight';
-				// добавить класс активности
-			}else {
-				// добавляем псевдо-активный класс
-				card.classList.add('card--active-first');
-				weights[index].classList.add('card__weight--active');
-				// когда мышь уйдет, вешаем настоящий активный класс
-				card.addEventListener('mouseleave', function handler() {
-					card.className = 'card card--active';
-					card.removeEventListener('mouseleave', handler);
-				});
-			}
-		});
+const changeCardState = function (card, index) {
+	if (!card.classList.contains('cards__card_disabled')) {
+		lastClickedCard = card;
+		if (card.classList.contains('cards__card_active') ||
+			card.classList.contains('cards__card_active-first')) {
+			card.className = 'cards__card';
+			weights[index].className = 'cards__weight';
+			card.removeEventListener('mouseleave', handler);
+		}else {
+			card.classList.add('cards__card_active-first');
+			weights[index].classList.add('cards__weight_active');
+			card.addEventListener('mouseleave', handler);
+		}
 	}
+};
+
+const changeCardStateFromLink = function (link) {
+	cards[link.getAttribute('data-link') - 1].className = 'cards__card cards__card_active';
+};
+
+cards.forEach(function (card, index) {
+	card.addEventListener('click', function () {
+		changeCardState(card, index);
+	});
+	card.addEventListener('keydown', function (e) {
+		if (document.activeElement === card && e.code === 'Enter') {
+			changeCardState(card, index);
+		}
+	});
 });
 
 links.forEach(function (link) {
 	link.addEventListener('click', function (e) {
 		e.preventDefault();
-		cards[link.getAttribute('data-link') - 1].className = 'card card--active';
+		changeCardStateFromLink(link);
 	});
 });
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,3 @@
+import '../blocks/utils/utils.js';
 import '../blocks/cards/cards.js';
+
